5bf43d90f45383a565af2604329374f92cda853c
Brendon <brendon.biagi@gmail.com>
2020-11-06 15:23:17 -0500

    fix hover effect?

--- hover.js
+++ hover.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
         info.style.display = "block";
     })
 
-    code.addEventListener('mouseleave', e=> {
+    code.addEventListener('mouseout', e=> {
         e.preventDefault();
         info.style.display = "hidden";
     })
