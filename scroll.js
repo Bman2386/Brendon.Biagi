878281cd755d2faec980af98521f03d5cc2df615
Brendon <brendon.biagi@gmail.com>
2021-06-14 19:05:03 -0400

    remove unnecissary comments

--- scroll.js
+++ scroll.js
@@ -1,20 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-
-//     const code = document.getElementById('code');
-//     const info = document.getElementById('codet');
-
-//     code.addEventListener('mouseover', e=> {
-//         e.preventDefault();
-//         // debugger
-//         info.style.display = "block";
-//     })
-
-//     code.addEventListener('mouseout', e=> {
-//         e.preventDefault();
-//         // debugger
-//         info.style.display = "hidden";
-//     })
-// })
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
