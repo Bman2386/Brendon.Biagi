8d3e57bb0482d5720ce20389415936d309502d6b
Brendon <brendon.biagi@gmail.com>
2023-07-10 15:21:40 -0400

    fix typo line 5

--- scroll.js
+++ scroll.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   const elementArray = ["top", "a1", "a2", "a3", "a4", "a5"];
   const classListArray = ['top-s', 'nav-s', 'nav-s','nav-s','nav-s', 'nav-s'];
-  if (document.body.scrollTop > 50 || document.documentElementArray.scrollTop > 50) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     for (let i = 0; i < elementArray.length; i++){
       document
       .getElementById(elementArray[i])
