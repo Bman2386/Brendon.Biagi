05e12721be31ff3a3f845268ab6a3f677698cd5c
Brendon <brendon.biagi@gmail.com>
2023-07-10 14:54:35 -0400

    rename arrays to more explicit names

--- scroll.js
+++ scroll.js
@@ -1,18 +1,18 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-  const element = ["top", "a1", "a2", "a3", "a4", "a5"];
-  const className = ['top-s', 'nav-s', 'nav-s','nav-s','nav-s', 'nav-s'];
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    for (let i = 0; i < element.length; i++){
+  const elementArray = ["top", "a1", "a2", "a3", "a4", "a5"];
+  const classListArray = ['top-s', 'nav-s', 'nav-s','nav-s','nav-s', 'nav-s'];
+  if (document.body.scrollTop > 50 || document.documentElementArray.scrollTop > 50) {
+    for (let i = 0; i < elementArray.length; i++){
       document
-      .getElementById(element[i])
-      .classList.add(className[i]);
+      .getElementById(elementArray[i])
+      .classList.add(classListArray[i]);
     }
   } else {
-    for (let i = 0; i < element.length; i++){
+    for (let i = 0; i < elementArray.length; i++){
       document
-      .getElementById(element[i])
-      .classList.remove(className[i]);
+      .getElementById(elementArray[i])
+      .classList.remove(classListArray[i]);
     }
   }
 }
